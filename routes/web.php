e67e37eab3f8cb0a339981d0d6b014655caa6e00
Mexidense <mexidense@gmail.com>
2018-11-20 21:23:20 +0100

    group route created and PostController created

--- routes/web.php
+++ routes/web.php
@@ -10,45 +10,54 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//
+//
+//Route::get('/hello', function (){
+//   return "Hello world";
+//});
+//
+//Route::get('url', function (){
+//    $url = url('url');
+//
+//    return "You are in " . $url;
+//});
+//
+////Adding a new pattern for id of type integer
+//Route::pattern('id', '\d+');
+//
+//Route::get('welcome/{user}/{id}', function ($user, $id) {
+//    return 'Welcome ' . $user . ', your ID is: ' . $id;
+//});
+//
+//Route::get('welcome/{user?}', function ($user = null) {
+//    $messenge = 'Welcome ';
+//    if($user === null){
+//        $messenge .= 'anonymous';
+//    }
+//    else{
+//        $messenge .= $user;
+//    }
+//    return $messenge;
+//});
+//
+//Route::get('url/{id}/profile', [
+//    'as' => 'profile',
+//    function($id){
+//        $url = route('profile', [
+//            'id' => $id
+//        ]);
+//        return "Retrieving profile in $url";
+//    }
+//]);
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', function (){
-   return "Hello world";
+Route::group(['prefix' => 'user'], function () {
+    Route::get('/', function(){
+        return 'This is route: /user';
+    });
+    Route::get('profile', function(){
+        return 'This is route: user/profile';
+    });
 });
-
-Route::get('url', function (){
-    $url = url('url');
-
-    return "You are in " . $url;
-});
-
-//Adding a new pattern for id of type integer
-Route::pattern('id', '\d+');
-
-Route::get('welcome/{user}/{id}', function ($user, $id) {
-    return 'Welcome ' . $user . ', your ID is: ' . $id;
-});
-
-Route::get('welcome/{user?}', function ($user = null) {
-    $messenge = 'Welcome ';
-    if($user === null){
-        $messenge .= 'anonymous';
-    }
-    else{
-        $messenge .= $user;
-    }
-    return $messenge;
-});
-
-Route::get('url/{id}/profile', [
-    'as' => 'profile',
-    function($id){
-        $url = route('profile', [
-            'id' => $id
-        ]);
-        return "Retrieving profile in $url";
-    }
-]);
