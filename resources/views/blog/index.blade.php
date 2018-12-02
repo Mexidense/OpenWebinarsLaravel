a1be85f06e9c73658deade6e318144bf928098bb
Mexidense <mexidense@gmail.com>
2018-12-02 01:17:39 +0100

    Blog layout and sections created

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -1,11 +1,18 @@
-<h1>Hello OpenWebinars</h1>
-@if (count($posts)>0)
-    <h4>Listado de posts:</h4>
-    <ul>
-        @foreach($posts as $post)
-            <li>{{ $post->title }}</li>
-        @endforeach
-    </ul>
-@else
-    <h3>No hay posts</h3>
-@endif
+@extends('blog.layout')
+
+@section('content')
+    @if (count($posts)>0)
+        <h4>Listado de posts:</h4>
+        <ul>
+            @foreach($posts as $post)
+                <li>{{ $post->title }}</li>
+            @endforeach
+        </ul>
+    @else
+        <h3>No hay posts</h3>
+    @endif
+@endsection
+
+@section('footer')
+    <h5>This is footer.</h5>
+@endsection
---
+++ resources/views/blog/layout.blade.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>Openwebinars Course!!!</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2">
+                    <h1>OpenWebinars Laravel</h1>
+                    @yield('content')
+                </div>
+            </div>
+        </div>
+        <footer>@yield('footer')</footer>
+    </body>
+</html>
