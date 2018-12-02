1224e16d26144627f7c129b0c4eed2cf5731fe59
Mexidense <mexidense@gmail.com>
2018-12-02 01:08:15 +0100

    post listing on homepage view

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -1,1 +1,11 @@
 <h1>Hello OpenWebinars</h1>
+@if (count($posts)>0)
+    <h4>Listado de posts:</h4>
+    <ul>
+        @foreach($posts as $post)
+            <li>{{ $post->title }}</li>
+        @endforeach
+    </ul>
+@else
+    <h3>No hay posts</h3>
+@endif
